3e297009f1baacb87e6594c5e73d2a177f225166
muralii <muralii@LAPTOP-3LTARU6N>
2024-06-30 12:39:34 +0530

    fourth commit

---
+++ corejava/src/com/dileep/assignment3/MultipleInheritance.java
@@ -0,0 +1,12 @@
+package com.dileep.assignment3;
+
+public class MultipleInheritance {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		State st=new State("Nile","Mtpiku","mon","liberty","ganga","kanchajunga","tajmahal","Kaveri","himadGopalswamy");
+		System.out.println(st);
+
+	}
+
+}
