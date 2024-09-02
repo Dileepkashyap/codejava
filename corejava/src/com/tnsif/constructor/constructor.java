4638b8097efe44703b9d4f275d496bab12545b6d
muralii <muralii@LAPTOP-3LTARU6N>
2024-09-02 21:21:52 +0530

    7th commit

---
+++ corejava/src/com/tnsif/constructor/constructor.java
@@ -0,0 +1,13 @@
+package com.tnsif.constructor;
+
+public class constructor {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Customer ob = new Customer();  //Constructor called
+		ob.setId(12);
+		ob.setName("dileep");
+		
+		System.out.println(ob);
+	}
+
+}
