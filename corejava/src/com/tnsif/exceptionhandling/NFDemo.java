ae9e6de2ef7ae36aa5818406246d625117c80b4e
muralii <muralii@LAPTOP-3LTARU6N>
2024-09-02 21:57:25 +0530

    8th commit

---
+++ corejava/src/com/tnsif/exceptionhandling/NFDemo.java
@@ -0,0 +1,9 @@
+package com.tnsif.exceptionhandling;
+
+public class NFDemo {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		NestedTry.chack();
+	}
+
+}
---
+++ corejava/src/com/tnsif/exceptionhandling/TryCatch.java
@@ -0,0 +1,19 @@
+package com.tnsif.exceptionhandling;
+
+public class TryCatch {
+	public static void divide() {
+		int a=6, b=0, c;
+		try {
+			c=a/b;
+		}
+		catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("Exception caught: "+e.getMessage());
+		}
+		catch(ArithmeticException e) {
+			System.out.println("Exception caught: "+e.getMessage());
+		}
+		catch(Exception e) {
+			System.out.println("Exception caught: "+e.getMessage());
+		}
+	}
+}
